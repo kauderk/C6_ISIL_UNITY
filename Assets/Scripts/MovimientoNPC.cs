0f6935b6ed5edbbeb23cdcf7d5341283fff444ab
kauderk <65237382+kauderk@users.noreply.github.com>
2022-05-07 07:53:08 -0500

    supress Variable is assigned but its value is never used on MovimientoNPC.cs

--- Assets/Scripts/MovimientoNPC.cs
+++ Assets/Scripts/MovimientoNPC.cs
@@ -6,7 +6,9 @@ public class MovimientoNPC : MonoBehaviour
 {
     // -----    SEEK -------- //
     [SerializeField] private float velocidadMovimiento = 15;
+#pragma warning disable CS0414 // Variable is assigned but its value is never used
     [SerializeField] private float velocidadRotacion = 3;
+#pragma warning restore CS0414    // restore value not used warning
     [SerializeField] private Transform target;
 
 
