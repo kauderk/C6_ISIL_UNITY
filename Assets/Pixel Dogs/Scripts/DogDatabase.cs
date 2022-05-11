635a9d0d5164a79ec13b27d482ac827e63415852
kauderk <65237382+kauderk@users.noreply.github.com>
2022-05-11 10:13:12 -0500

    -a PixelDog package | -m and placed inside s6-dv3d-scene

---
+++ Assets/Pixel Dogs/Scripts/DogDatabase.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DogDatabase : MonoBehaviour
+{
+
+    public List<Dog> dogDatabase = new List<Dog>();
+}
