656ffc5a38b14f089589497cc346e64cf4dafe24
kauderk <65237382+kauderk@users.noreply.github.com>
2022-05-11 10:34:54 -0500

    comment out Debug.Logs on ForestDemo and PixelDogs

--- Assets/Pixel Dogs/Scripts/ObsticleDetection.cs
+++ Assets/Pixel Dogs/Scripts/ObsticleDetection.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ObsticleDetection : MonoBehaviour {
+public class ObsticleDetection : MonoBehaviour
+{
 
     DogBehaviour behaviourScript;
 
@@ -22,7 +23,7 @@ public class ObsticleDetection : MonoBehaviour {
 
         if (behaviourScript.isSniffing)
         {
-            Debug.DrawRay(new Vector2(transform.position.x + xOffset, transform.position.y - 0.15f), new Vector3(0, -0.2f, 0), Color.green, 0.05f);
+            //Debug.DrawRay(new Vector2(transform.position.x + xOffset, transform.position.y - 0.15f), new Vector3(0, -0.2f, 0), Color.green, 0.05f);
             RaycastHit2D searchHit = Physics2D.Raycast(new Vector2(transform.position.x + xOffset, transform.position.y - 0.15f), new Vector2(0, -0.2f), 1);
             if (searchHit.collider != null)
             {
@@ -36,13 +37,13 @@ public class ObsticleDetection : MonoBehaviour {
 
         if (behaviourScript.isLooking)
         {
-            Debug.DrawRay(transform.position, facingDirection, Color.green, 0.05f);
+            //Debug.DrawRay(transform.position, facingDirection, Color.green, 0.05f);
             RaycastHit2D lookHit = Physics2D.Raycast(transform.position, facingDirection, 1);
             if (lookHit.collider != null)
             {
                 if (lookHit.collider.tag == "Treasure" && lookTarget == "Treasure")
                 {
-                    Debug.Log(this.name + " Found Treasure!");
+                    //Debug.Log(this.name + " Found Treasure!");
                     Destroy(lookHit.transform.gameObject);
                     ToggleLooking(false, "none");
 
@@ -51,29 +52,29 @@ public class ObsticleDetection : MonoBehaviour {
                 }
                 else if (lookHit.collider.tag == "FetchMachine" && lookTarget == "FetchMachine")
                 {
-                    Debug.Log(this.name + " Found the Fetch Machine!");
+                    //Debug.Log(this.name + " Found the Fetch Machine!");
                     ToggleLooking(false, "none");
                     if (behaviourScript.objective == "PlayFetch")
                     {
-                        Debug.Log(this.name + " wants to play fetch");
+                        //Debug.Log(this.name + " wants to play fetch");
                         behaviourScript.Laydown();
                         behaviourScript.Bark();
                         StartCoroutine(WaitForCannon(lookHit.transform));
                     }
                     else if (behaviourScript.objective == "ReturnBall")
                     {
-                        Debug.Log(this.name + " returns the ball!");
+                        //Debug.Log(this.name + " returns the ball!");
                         behaviourScript.DropBall();
                     }
-                    
+
                 }
-            }   
+            }
         }
 
         //wall detection always on
         facingDirection = behaviourScript.FacingDirection2D();
 
-        Debug.DrawRay(transform.position, facingDirection, Color.green, 0.05f);
+        //Debug.DrawRay(transform.position, facingDirection, Color.green, 0.05f);
         RaycastHit2D hit = Physics2D.Raycast(transform.position, facingDirection, 1);
         if (hit.collider != null)
         {
@@ -82,7 +83,7 @@ public class ObsticleDetection : MonoBehaviour {
                 behaviourScript.Flip();
                 return;
             }
-        }        
+        }
     }
 
     public void ToggleSniffing(bool mode, string target_m)
@@ -93,8 +94,8 @@ public class ObsticleDetection : MonoBehaviour {
 
     public void ToggleLooking(bool mode, string target_m)
     {
-            lookTarget = target_m;
-            behaviourScript.isLooking = mode;
+        lookTarget = target_m;
+        behaviourScript.isLooking = mode;
     }
 
     public IEnumerator WaitForCannon(Transform fetchMachine)
@@ -102,7 +103,7 @@ public class ObsticleDetection : MonoBehaviour {
         yield return new WaitForSeconds(2f);
         GameObject ball = fetchMachine.GetChild(0).GetComponent<ItemCannon>().Fire();
         yield return new WaitForSeconds(0.1f);
-        if(transform.position.x < ball.transform.position.x)
+        if (transform.position.x < ball.transform.position.x)
         {
             if (!behaviourScript.mySprite.flipX)
             {
@@ -111,6 +112,6 @@ public class ObsticleDetection : MonoBehaviour {
         }
         ToggleSniffing(true, "Ball");
         behaviourScript.StartRunning();
-        Debug.Log(this.name + " is chasing the ball!");
+        //Debug.Log(this.name + " is chasing the ball!");
     }
 }
