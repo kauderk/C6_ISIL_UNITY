ace2d926c7816998ef8c8f76839f7b8c5b5d9073
kauderk <65237382+kauderk@users.noreply.github.com>
2022-05-11 06:36:50 -0500

    before s6-dv3d-scene

--- Assets/Networking/Scripts/UIEvents.cs
+++ Assets/Networking/Scripts/UIEvents.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
+using UnityEngine.Events;
 using TMPro;
 
 public class UIEvents : MonoBehaviour
 {
+    public UnityEvent OnEnableEvent;
+    public void OnEnable()
+    {
+        OnEnableEvent?.Invoke();
+    }
+
+
     public void SetText(string text)
     {
         gameObject.GetComponent<TMP_Text>().text = text;
